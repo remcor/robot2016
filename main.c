bfaf3cbee2a08e51c0bfa764d19a991b39cba1c6
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2016-10-11 16:26:27 -0500

    Initial code from last year

---
+++ main.c
@@ -0,0 +1,83 @@
+#pragma config(Motor, port3, leftWheel, tmotorVex393_MC29, openLoop)
+#pragma config(Motor, port4, rightWheel, tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor, port5, handLeft, tmotorServoStandard, openLoop)
+#pragma config(Motor, port7, handRight, tmotorServoStandard, openLoop)
+#pragma config(Motor, port8, handMotor, tmotorVex393_MC29, openLoop)
+#pragma config(Motor, port9, armMotor, tmotorVex269_MC29, openLoop)
+
+/******************************************
+Joystick mapping:
+	Ch3 - Left Joystick up/down
+	Ch2 - Right Joystick up/down
+Buttons mapping:
+	Btn5U - Hand Rotate Left
+	Btn6U - Hand Rotate Right
+	Btn5D - Raise Arm
+	Btn6D - Lower Arm
+	Btn8D - Open Hand
+	Btn8U - Closes Hand
+******************************************/
+
+task main ()
+{
+	int maxServoPos = 127;
+	int maxServoNeg = -127;
+	int servoIncrement = 10;
+
+	int threshold = 10;
+
+	while (true) {
+
+		// If greater then threshold set, otherwise set to 0
+		if (vexRT[Ch3] > threshold || vexRT[Ch3] < -threshold) {
+			motor[leftWheel] = vexRT[Ch3];   // Left Joystick Y value
+		} else {
+			motor[leftWheel] = 0;
+		}
+
+		// If greater then threshold set, otherwise set to 0
+		if (vexRT[Ch2] > threshold || vexRT[Ch2] < -threshold) {
+			motor[rightWheel] = vexRT[Ch2];   // Right Joystick Y value
+		} else {
+			motor[rightWheel] = 0;
+		}
+
+		// Open hand
+		if (vexRT[Btn8D] == 1) {
+
+			if (motor[handLeft] < maxServoPos) {
+				motor[handLeft] = motor[handLeft] + servoIncrement;
+				motor[handRight] = motor[handRight] - servoIncrement;
+			}
+		}
+
+		// Close Hand
+		if (vexRT[Btn8U] == 1) {
+
+			if (motor[handLeft] > maxServoNeg ) {
+				motor[handLeft] = motor[handLeft] - servoIncrement;
+				motor[handRight] = motor[handRight] + servoIncrement;
+			}
+		}
+
+		// Button 5D raises arm
+		if (vexRT[Btn5D] == 1) {
+			motor[armMotor] = 100;
+		} else if (vexRT[Btn6D] == 1) {
+			motor[armMotor] = -100;
+		} else {
+			motor[armMotor] = 0;
+		}
+
+		// Button 5U rotates hand left
+		if (vexRT[Btn5U] == 1) {
+			motor[handMotor] = 100;
+		} else if (vexRT[Btn6U] == 1) {
+			motor[handMotor] = -100;
+		} else {
+			motor[handMotor] = 0;
+		}
+
+	}
+
+}
